6406b91d493d325e83924e96853a20e8276558b8
AudriusBartkus <audriusbart@gmail.com>
2014-03-29 14:16:24 +0200

    assets installed v1

---
+++ src/Cocktails/RecipesBundle/Admin/ImageAdmin.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Audrius
+ * Date: 14.3.29
+ * Time: 12.47
+ */
+
+namespace Cocktails\RecipesBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class ImageAdmin extends Admin
+{
+    // Fields to be shown on create/edit forms
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('title', 'text', array('label' => 'Post Title'))
+            ->add('author', 'entity', array('class' => 'Acme\DemoBundle\Entity\User'))
+            ->add('body') //if no type is specified, SonataAdminBundle tries to guess it
+        ;
+    }
+
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('title')
+            ->add('author')
+        ;
+    }
+
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('title')
+            ->add('slug')
+            ->add('author')
+        ;
+    }
+}
