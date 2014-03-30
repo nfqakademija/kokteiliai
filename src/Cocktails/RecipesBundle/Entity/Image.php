c4b05f9df73ab408782415b3f5db5a90e4b8e3a0
AudriusBartkus <audriusbart@gmail.com>
2014-03-30 20:11:06 +0300

    file upload tutorial

--- src/Cocktails/RecipesBundle/Entity/Image.php
+++ src/Cocktails/RecipesBundle/Entity/Image.php
@@ -1,65 +1,157 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Audrius
- * Date: 14.3.29
- * Time: 15.34
- */
 
 namespace Cocktails\RecipesBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
 
 /**
  * @ORM\Entity
- * @ORM\Table(name="image")
-  */
+ * @ORM\HasLifecycleCallbacks
+ */
 class Image
 {
+    private $temp;
+
     /**
-     * @ORM\Column(type="integer")
+     * Sets file.
+     *
+     * @param UploadedFile $file
+     */
+    public function setFile(UploadedFile $file = null)
+    {
+        $this->file = $file;
+        // check if we have an old image path
+        if (is_file($this->getAbsolutePath())) {
+            // store the old name to delete after the update
+            $this->temp = $this->getAbsolutePath();
+        } else {
+            $this->path = 'initial';
+        }
+    }
+
+    /**
+     * @ORM\PrePersist()
+     * @ORM\PreUpdate()
+     */
+    public function preUpload()
+    {
+        if (null !== $this->getFile()) {
+            $this->path = $this->getFile()->guessExtension();
+        }
+    }
+
+    /**
+     * @ORM\PostPersist()
+     * @ORM\PostUpdate()
+     */
+    public function upload()
+    {
+        if (null === $this->getFile()) {
+            return;
+        }
+
+        // check if we have an old image
+        if (isset($this->temp)) {
+            // delete the old image
+            unlink($this->temp);
+            // clear the temp image path
+            $this->temp = null;
+        }
+
+        // you must throw an exception here if the file cannot be moved
+        // so that the entity is not persisted to the database
+        // which the UploadedFile move() method does
+        $this->getFile()->move(
+            $this->getUploadRootDir(),
+            $this->id.'.'.$this->getFile()->guessExtension()
+        );
+
+        $this->setFile(null);
+    }
+
+    /**
+     * @ORM\PreRemove()
+     */
+    public function storeFilenameForRemove()
+    {
+        $this->temp = $this->getAbsolutePath();
+    }
+
+    /**
+     * @ORM\PostRemove()
+     */
+    public function removeUpload()
+    {
+        if (isset($this->temp)) {
+            unlink($this->temp);
+        }
+    }
+
+    public function getAbsolutePath()
+    {
+        return null === $this->path
+            ? null
+            : $this->getUploadRootDir().'/'.$this->id.'.'.$this->path;
+    }
+
+
+    /**
      * @ORM\Id
+     * @ORM\Column(type="integer")
      * @ORM\GeneratedValue(strategy="AUTO")
      */
-    protected $id;
+    public $id;
 
     /**
-     * @ORM\column(type="string", length=100)
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
      */
-    protected $name;
-
-
+    public $name;
 
     /**
-     * Get id
-     *
-     * @return integer 
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
-    public function getId()
+    public $path;
+
+
+    public function getWebPath()
     {
-        return $this->id;
+        return null === $this->path
+            ? null
+            : $this->getUploadDir().'/'.$this->path;
+    }
+
+    protected function getUploadRootDir()
+    {
+        // the absolute directory path where uploaded
+        // documents should be saved
+        return __DIR__.'/../../../../web/'.$this->getUploadDir();
+    }
+
+    protected function getUploadDir()
+    {
+        // get rid of the __DIR__ so it doesn't screw up
+        // when displaying uploaded doc/image in the view.
+        return 'uploads/documents';
     }
 
     /**
-     * Set name
-     *
-     * @param string $name
-     * @return Image
+     * @Assert\File(maxSize="6000000")
      */
-    public function setName($name)
-    {
-        $this->name = $name;
-
-        return $this;
-    }
+    private $file;
 
     /**
-     * Get name
+     * Sets file.
      *
-     * @return string 
+     * @param UploadedFile $file
      */
-    public function getName()
+
+    public function getFile()
     {
-        return $this->name;
+        return $this->file;
     }
+
 }
