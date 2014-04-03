9da1a1e89e4368b76fe40efd91f5c0782a7686f4
rokasra <rokas>
2014-04-03 17:56:50 +0300

    Receotu tipai entity

---
+++ src/Cocktails/RecipesBundle/Entity/RecipeType.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Cocktails\RecipesBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * RecipeTypes
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Cocktails\RecipesBundle\Entity\RecipeTypesRepository")
+ */
+class RecipeType
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=50)
+     */
+    private $name;
+
+    /*
+     *@ORM\OneToMany(targetEntity="Recipe", mappedBy="recipeType")
+     */
+    private $recipes;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return RecipeType
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->recipes = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Add recipes
+     *
+     * @param \Cocktails\RecipesBundle\Entity\Recipe $recipes
+     * @return RecipesType
+     */
+    public function addRecipe(\Cocktails\RecipesBundle\Entity\Recipe $recipes)
+    {
+        $this->recipes[] = $recipes;
+
+        return $this;
+    }
+
+    /**
+     * Remove recipes
+     *
+     * @param \Cocktails\RecipesBundle\Entity\Recipe $recipes
+     */
+    public function removeRecipe(\Cocktails\RecipesBundle\Entity\Recipe $recipes)
+    {
+        $this->recipes->removeElement($recipes);
+    }
+
+    /**
+     * Get recipes
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getRecipes()
+    {
+        return $this->recipes;
+    }
+}
