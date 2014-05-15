03794819189f66c67d4d7a94ee34cda6ea0c0cfa
AudriusBartkus <audriusbart@gmail.com>
2014-05-16 00:47:16 +0300

    veikiantys mygtukai

--- src/Cocktails/RecipesBundle/Controller/NavigationController.php
+++ src/Cocktails/RecipesBundle/Controller/NavigationController.php
@@ -44,32 +44,36 @@ class NavigationController extends Controller
 
     public function showRecipeAction($id){
         $recipe = $this->getDoctrine()->getRepository('CocktailsRecipesBundle:Recipe')->find($id);
-
-        $usr = $this->getUser()->getId();
-        if($this->getDoctrine()->getRepository('CocktailsRecipesBundle:UsersRecipes')->findOneBy(array('recipe'=>$id, 'user'=>$usr)))
-        {
-            $added = true;
-        } else {
-            $added = false;
-        }
-        $ingredients = $this->getDoctrine()->getRepository('CocktailsRecipesBundle:RecipesIngredients')->findBy(array('recipe'=>$recipe->getId()));
-        $userIngredients = $this->getDoctrine()->getRepository('CocktailsRecipesBundle:UsersIngredients')->findBy(array('user'=>$usr));
         $ingredientsNeeded = array();
-        foreach ($ingredients as $ingredient){
-            foreach ($userIngredients as $userIngredient){
-                if ($ingredient->getIngredient() == $userIngredient->getIngredient()){
-                    if ($ingredient->getQuantity() > $userIngredient->getQuantity()){
+
+        $added = false;
+        $ingredients = $this->getDoctrine()->getRepository('CocktailsRecipesBundle:RecipesIngredients')->findBy(array('recipe'=>$recipe->getId()));
+        if( $this->container->get('security.context')->isGranted('IS_AUTHENTICATED_FULLY') ){
+            $usr = $this->getUser()->getId();
+            if($this->getDoctrine()->getRepository('CocktailsRecipesBundle:UsersRecipes')->findOneBy(array('recipe'=>$id, 'user'=>$usr)))
+            {
+                $added = true;
+
+            }
+            $userIngredients = $this->getDoctrine()->getRepository('CocktailsRecipesBundle:UsersIngredients')->findBy(array('user'=>$usr));
+            foreach ($ingredients as $ingredient){
+                $quantity = $ingredient->getQuantity();
+                $IngrNeeded = array('name'=>$ingredient->getIngredient()->getName(),
+                    'quantity'=>$quantity,
+                    'measureUnit'=>$ingredient->getIngredient()->getMeasureUnit()->getName()
+                );
+                foreach ($userIngredients as $userIngredient){
+                    if ($ingredient->getIngredient() == $userIngredient->getIngredient()){
                         $quantity = $ingredient->getQuantity() - $userIngredient->getQuantity();
-                        $IngrNeeded = array('name'=>$ingredient->getIngredient()->getName(),
-                                            'quantity'=>$quantity,
-                                            'measureUnit'=>$ingredient->getIngredient()->getMeasureUnit()->getName()
-                        );
-                        $ingredientsNeeded[] = $IngrNeeded;
+                        $IngrNeeded['quantity'] = $quantity;
                     }
+                }
+                if ($quantity > 0){
+                    $ingredientsNeeded[] = $IngrNeeded;
                 }
             }
         }
-        return $this->render('CocktailsRecipesBundle:Default:recipeSingleWindow.html.twig', array('recipe'=>$recipe, 'ingredients'=>$ingredients, 'userIngredients'=>$userIngredients, 'ingredientsNeeded'=>$ingredientsNeeded, 'added'=>$added));
+        return $this->render('CocktailsRecipesBundle:Default:recipeSingleWindow.html.twig', array('recipe'=>$recipe, 'ingredients'=>$ingredients, 'ingredientsNeeded'=>$ingredientsNeeded, 'added'=>$added));
     }
 
     public function typeSortAction()
